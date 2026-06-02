f4a840a4a6702dd7ec846b12232dffb5a5655f14
qvbne <qvbnee@gmail.com>
2026-06-02 18:10:18 +0700

    prepare render deploy

--- PobedaB2B/settings.py
+++ PobedaB2B/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lzt+ywi!x%jv6=3+mi7q4q9arc$%+0*3svd1sj5b%1au=c1wnq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
